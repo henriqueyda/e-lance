a781eb35b660e3c0307a2e329cda6f7df095a0ca
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-04-27 23:34:50 -0300

    add mask do jquery para validacao dos campos do form

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,18 +1,77 @@
 $(function () {
 
-    $('form').on('submit', function (e) {
+  $('form').on('submit', function (e) {
 
-      e.preventDefault();
+    e.preventDefault();
 
-      $.ajax({
-        type: 'post',
-        url: 'https://feegcmfjie.execute-api.us-east-1.amazonaws.com/versao1',
-        data: $('form').serialize(),
-        success: function () {
-          alert('form was submitted');
-        }
-      });
-
+    $.ajax({
+      type: 'post',
+      url: 'https://feegcmfjie.execute-api.us-east-1.amazonaws.com/versao1',
+      data: $('form').serialize(),
+      success: function () {
+        alert('form was submitted');
+      }
     });
+
+  });
+
+});
+
+//mask para campo nome somente letras com acentos
+$('#nome').keyup(function () {
+  this.value = this.value.replace(/[^A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]/g, '');
+});
+
+//mask para campo sobrenome somente letras com acentos
+$('#sobrenome').keyup(function () {
+  this.value = this.value.replace(/[^A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]/g, '');
+});
+
+//mask para o campo cep
+$('#cep').on('input', function () {
+  $('#cep').mask("00.000-000");
+});
+
+$('#cep').blur(function () {
+  // debugger
+  let cep = $('#cep').val();
+  cep = cep.replace('.', '').replace('-', '')
+  $.ajax({
+    url: 'https://viacep.com.br/ws/' + cep + '/json/',
+    method: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      if (data.erro != 'true') {
+        alert('deu certo')
+        console.log(data)
+        $('#cidade').val(data.localidade);
+        $('#estado').val(data.uf);
+      } else {
+        alert('Cep não encontrado! Digite um cep válido.');
+        $('#cep').val('');
+      }
+    }
+  })
+});
+
+//mask para email
+$('#email').keyup(function () {
+  // debugger
+  $('#email').mask("A", {
+    translation: {
+      "A": { pattern: /[\w@\-.+]/, recursive: true }
+    }
+  });
+});
+
+$('#confimarSenha').blur(function () {
+  if ($('#confimarSenha').val() != $('#senha').val()) {
+    alert('As senha não se condizem.');
+    $('#confimarSenha').val('');
+    $('#senha').val('');
+    $('#btnSubmit').attr('disabled', true);
+  } else {
+    $('#btnSubmit').attr('disabled', false);
+  }
 
 });
