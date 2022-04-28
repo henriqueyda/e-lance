f0f87d2368c870733f3c225d413957edaaa1917c
henriqueyda <henrique.ydaa@gmail.com>
2022-04-27 22:34:31 -0300

    feat:adicionando código de cadastro

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -8,8 +8,14 @@ $(function () {
         type: 'post',
         url: 'https://feegcmfjie.execute-api.us-east-1.amazonaws.com/versao1',
         data: $('form').serialize(),
-        success: function () {
-          alert('form was submitted');
+        success: function (response) {
+            if(response.statusCode == "409"){
+                alert("Email já cadastrado! Tente novamente")
+            }
+            if(response.statusCode == "200"){
+                alert("Usuário cadastrado com sucesso!")
+                window.location.reload()
+            }
         }
       });
 
