a8306064cd56dee29b7aa076b8e9b6f36db0ce40
henriqueyda <henrique.ydaa@gmail.com>
2022-05-17 21:16:43 -0300

    adicionando logout

---
+++ scripts/logout.js
@@ -0,0 +1,18 @@
+document.getElementById("logout").onclick = (e) =>{
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split( ';' );
+    let email;
+    for( let i = 0; i < ca.length; i++ ){
+        if( ca[i].indexOf( "username=" ) == 0 ){
+            email = ca[i].substring( "username=".length, ca[i].length );
+        }
+    }
+    if(email != null){
+        document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        alert('Usuário deslogado com sucesso!');
+    }
+    else{
+        alert('Já está deslogado!');
+        e.preventDefault();
+    }
+}
