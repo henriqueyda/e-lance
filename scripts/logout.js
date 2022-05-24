ddfc9c6e5283743d482c9f6533c0a1f9894d47b8
henriqueyda <henrique.ydaa@gmail.com>
2022-05-23 23:19:42 -0300

    corrige para deslogar cookie tipo

--- scripts/logout.js
+++ scripts/logout.js
@@ -9,6 +9,7 @@ document.getElementById("logout").onclick = (e) =>{
     }
     if(email != null){
         document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.cookie = "tipo=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
         alert('Usuário deslogado com sucesso!');
         //limpar o cookie do name e header
         document.location.reload(true);
