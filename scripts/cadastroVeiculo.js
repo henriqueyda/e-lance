c1e839f003207827490b68bea814ffa408b16fc2
henriqueyda <henrique.ydaa@gmail.com>
2022-05-23 23:16:24 -0300

    adiciona código de cadastro de veículos

--- scripts/cadastroVeiculo.js
+++ scripts/cadastroVeiculo.js
@@ -1,3 +1,37 @@
+$(function () {
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split( ';' );
+  let email;
+  for( let i = 0; i < ca.length; i++ ){
+      if( ca[i].indexOf( "username=" ) == 0 ){
+          email = ca[i].substring( "username=".length, ca[i].length );
+      }
+  }
+
+  if(email != null){
+    $('form').on('submit', function (e) {
+      e.preventDefault();
+  
+      $.ajax({
+        type: 'post',
+        url: 'https://9onbskhs1i.execute-api.us-east-1.amazonaws.com/versao1',
+        data: $('form').serialize() + "&email=" + email,
+        success: function (response) {
+          if(response.statusCode == "400"){
+            alert("Erro ao cadastrar veículo!")
+          }
+          if(response.statusCode == "200"){
+            alert("Veículo cadastrado com sucesso!")
+            window.location.reload()
+          }
+        }
+      });
+  
+    });
+  }
+  
+  });
+
 $("#url").blur(function(e){
     // debugger
     if ($("#url").val() != '') {
