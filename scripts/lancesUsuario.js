34af7a5c7be498d681f75c041213ed2cc217e2d0
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-05-24 22:13:51 -0300

    ajuste de url

--- scripts/lancesUsuario.js
+++ scripts/lancesUsuario.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $.ajax({
-        url: 'https://q06uzv22s4.execute-api.us-east-1.amazonaws.com/testeinicial/',
+        url: 'https://5uf2ge170m.execute-api.us-east-1.amazonaws.com/versao1',
         method: 'GET',
-        ContentType: "application/json;charset=UTF-8",
+        contentType: "application/json;charset=UTF-8",
         sucess: function (data) {
             console.log(data)
         }
