5602325d6fbc6cf6d677a3fa77d6944f763a33c1
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-05-24 22:39:04 -0300

    ajuste na type method

--- scripts/lancesUsuario.js
+++ scripts/lancesUsuario.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
     $.ajax({
-        url: 'https://d90i52trsl.execute-api.us-east-1.amazonaws.com/versao1',
-        method: 'POST',
-        data: JSON.stringify($("#email_address").val()),
+        url: `https://d90i52trsl.execute-api.us-east-1.amazonaws.com/versao1?email=${$("#email_address").val()}`,
+        method: 'GET',
         contentType: "application/json;charset=UTF-8",
         sucess: function (data) {
             console.log(data)
