2dc779d3269ad125d54beba67933ddecf8bb2178
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-05-24 22:41:20 -0300

    ajuste de nomenclatura

--- scripts/lancesUsuario.js
+++ scripts/lancesUsuario.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         url: `https://d90i52trsl.execute-api.us-east-1.amazonaws.com/versao1?email=${$("#email_address").val()}`,
         method: 'GET',
         contentType: "application/json;charset=UTF-8",
-        sucess: function (data) {
+        success: function (data) {
             console.log(data)
         }
     });
