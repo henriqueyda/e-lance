3b29894b36f066b60020e2475569ee9a8ddd9c13
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-05-24 22:52:44 -0300

    ajuste name do id

--- scripts/lancesUsuario.js
+++ scripts/lancesUsuario.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-    console.log($("#email_address").val());
     $.ajax({
-        url: `https://d90i52trsl.execute-api.us-east-1.amazonaws.com/versao1?email=${$("#email_address").val()}`,
+        url: `https://d90i52trsl.execute-api.us-east-1.amazonaws.com/versao1?email=${$("#email").val()}`,
         method: 'GET',
         contentType: "application/json;charset=UTF-8",
         success: function (data) {
