0a97f8d7f846c6a6964cca66143fb21892a7fdac
Aléxia Vitória Melhado Pereira <aleeevitoria@gmail.com>
2022-05-24 22:34:45 -0300

    ajuste na chamada do id email_address

--- scripts/lancesUsuario.js
+++ scripts/lancesUsuario.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $.ajax({
         url: 'https://d90i52trsl.execute-api.us-east-1.amazonaws.com/versao1',
         method: 'POST',
-        data: JSON.stringify({"email": document.getElementById("email_address").value}),
+        data: JSON.stringify($("#email_address").val()),
         contentType: "application/json;charset=UTF-8",
         sucess: function (data) {
             console.log(data)
